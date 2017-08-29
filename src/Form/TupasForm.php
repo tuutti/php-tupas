afdd43d894e952a50494de4f5af5ddfde38b4daf
Bart Feenstra <bart@mynameisbart.com>
2017-08-29 16:23:28 +0200

    Allow transaction date-times to be specified explicitly as well.

--- src/Form/TupasForm.php
+++ src/Form/TupasForm.php
@@ -11,6 +11,13 @@ class TupasForm implements TupasFormInterface
     use TupasEncryptionTrait;
 
     /**
+     * The transaction date-time.
+     *
+     * @var \DateTime
+     */
+    protected $dateTime;
+
+    /**
      * The transaction id.
      *
      * @var string
@@ -60,15 +67,17 @@ class TupasForm implements TupasFormInterface
     protected $bank;
 
     /**
-     * TupasForm constructor.
+     * Constructs a new instance.
      *
      * @param BankInterface $bank
      *   The bank.
+     * @param \DateTime $dateTime
+     *   The transaction date-time or NULL to use the current date-time.
      */
-    public function __construct(BankInterface $bank, $defaultLanguage = 'en')
+    public function __construct(BankInterface $bank, \DateTime $dateTime = null)
     {
         $this->bank = $bank;
-        $this->setLanguage($defaultLanguage);
+        $this->dateTime = $dateTime ?: new \DateTime();
     }
 
     /**
@@ -197,7 +206,7 @@ class TupasForm implements TupasFormInterface
      */
     public function getStamp()
     {
-        return sprintf('%s%s', (new \DateTime())->format('YmdHis'), $this->getTransactionId());
+        return sprintf('%s%s', ($this->dateTime)->format('YmdHis'), $this->getTransactionId());
     }
 
     /**
